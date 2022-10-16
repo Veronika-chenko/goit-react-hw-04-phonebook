eb3d024c2a72610ba95556c86b6a645f07c3df3f
Veronika Kravchenko <aniregma@gmail.com>
2022-10-16 20:53:54 +0300

    refactoring
    create separate components(Form, Contacts)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,53 @@
 import { Component } from "react";
 // import { nanoid } from 'nanoid';
 import shortid from 'shortid';
+import { Form } from './Form/Form';
+import { ContactList } from './Contacts/Contacts'
+
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
-    number: ''
+    // contacts: [],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: ''
   }
 
-  handleInputChange = evt => {
-    const { name, value } = evt.currentTarget;
-    this.setState({ [name]: value })   
-  }
-
-  handleSubmit = evt => {
-    evt.preventDefault();
-    // прочитати name:
-    this.addTodo(this.state.name, this.state.number);
-    this.resetForm()
+  formSubmitHandler = data => {
+    setTimeout(() => {
+      // console.log(data)
+    }, 1000)
   }
 
   addTodo = (userNeme, userTel) => {
-    // console.log(inputText)
-    //create contact and add to state.contacts:
+    //create and add contact to state.contacts:
     const todo = {
       id: shortid.generate(),
-      contact: {name: userNeme, number: userTel},
-      // name: inputText,
+      name: userNeme,
+      number: userTel,
     };
-
     // update/add into array:
     this.setState(prevState => ({
       contacts: [todo, ...prevState.contacts]
     }))
   }
 
-  resetForm = () => {
-    this.setState({ name: '', number: '' });
-  }
-  
-
   render() {
     const { contacts} = this.state;
     return (
       <>
         <h2>Phonebook</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="text" style={{display: "block", marginBottom: "12px"}}>Name</label>
-          <input
-              id="text"
-              type="text"
-              name="name"
-              value={this.state.name}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              onChange={this.handleInputChange}  
-              required  
-          />
-          <label htmlFor="tel" style={{display: "block", marginBottom: "12px"}}>Number</label>
-          <input
-            id="tel"
-            type="tel"
-            name="number"
-            value={this.state.number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            onChange={this.handleInputChange} 
-            required
-          />
-          <button type="submit">Add contact</button>
-        </form>
+        {/* <Form onSubmit={this.formSubmitHandler} addTodo={this.addTodo} /> */}
+        <Form onSubmit={this.addTodo} />
         
         <h2>Contacts</h2>
-        <ul prop={contacts}>
-          {contacts.map((contact) => (
-            <li key={contact.id}>{contact.contact.name}: {contact.contact.number}</li>
-          ))}
-        </ul>
+        <p>Find contacts by name</p>
+        <input type="text" />
+        <ContactList contacts={contacts} />
       </>
     )
   }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+
+export class Form extends Component {
+    state = {
+        name: '',
+        number: ''
+    }
+
+    handleInputChange = evt => {
+        const { name, value } = evt.currentTarget;
+        this.setState({ [name]: value })   
+    }
+
+    handleSubmit = evt => {
+        evt.preventDefault();
+        // прочитати name:
+        this.props.onSubmit(this.state.name, this.state.number);
+        // this.props.onSubmit(this.state);
+        this.resetForm()
+    }
+
+    resetForm = () => {
+        this.setState({ name: '', number: '' });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+          <label htmlFor="text" style={{display: "block", marginBottom: "12px"}}>Name</label>
+          <input
+              id="text"
+              type="text"
+              name="name"
+              value={this.state.name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              onChange={this.handleInputChange}  
+              required  
+          />
+          <label htmlFor="tel" style={{display: "block", marginBottom: "12px"}}>Number</label>
+          <input
+            id="tel"
+            type="tel"
+            name="number"
+            value={this.state.number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            onChange={this.handleInputChange} 
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        )
+    }
+}
